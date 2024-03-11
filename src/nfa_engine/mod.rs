98b1e4342aba4b8dfb01bcb0d8060bbc790febf1
Ruberald <sajidhossain909@gmail.com>
2024-03-12 00:54:10 +0530

    Matcher trait and implementations

--- src/nfa_engine/mod.rs
+++ src/nfa_engine/mod.rs
@@ -1,14 +1,10 @@
-use std::collections::VecDeque;
+use std::{char, collections::VecDeque};
 
 struct State<'a> {
     name: String,
-    transitions: VecDeque<(&'a Matcher, &'a State<'a>)>,
+    transitions: VecDeque<(&'a dyn Matcher, &'a State<'a>)>,
     starts_group: Vec<&'static str>,
     ends_group: Vec<&'static str>,
-}
-
-struct Matcher {
-
 }
 
 impl<'a> State<'a> {
@@ -21,12 +17,69 @@ impl<'a> State<'a> {
         }
     }
 
-    fn add_transition(&mut self, to_state: &'a State, matcher: &'a Matcher) {
+    fn add_transition(&mut self, to_state: &'a State, matcher: &'a dyn Matcher) {
         self.transitions.push_back((matcher, to_state));
     }
 
-    fn pushfront_transition(&mut self, to_state: &'a State, matcher: &'a Matcher) {
+    fn pushfront_transition(&mut self, to_state: &'a State, matcher: &'a dyn Matcher) {
         self.transitions.push_front((matcher, to_state));
+    }
+}
+
+trait Matcher {
+    fn matches(&self, _ch: char) -> bool {
+        false
+    }
+
+    fn is_epsilon(&self) -> bool {
+        false
+    }
+
+    fn get_label(&self) -> Option<char> {
+        None
+    }
+}
+
+struct CharacterMatcher {
+    c: char,
+}
+
+impl Matcher for CharacterMatcher {
+
+    fn matches(&self, ch: char) -> bool {
+        self.c == ch
+    }
+
+    fn is_epsilon(&self) -> bool {
+        false
+    }
+
+    fn get_label(&self) -> Option<char> {
+        Some(self.c)
+    }
+}
+
+impl CharacterMatcher {
+    fn new(c: char) -> Self {
+        CharacterMatcher {
+            c,
+        }
+    }
+}
+
+struct EpsilonMatcher { }
+
+impl Matcher for EpsilonMatcher {
+    fn matches(&self, _ch: char) -> bool {
+        true
+    }
+
+    fn is_epsilon(&self) -> bool {
+        true
+    }
+
+    fn get_label(&self) -> Option<char> {
+        Some('c')
     }
 }
 
